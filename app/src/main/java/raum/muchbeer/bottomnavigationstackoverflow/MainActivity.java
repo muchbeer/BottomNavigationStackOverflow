48ea287d7886bab428e0a5273bb225e6be3bf723
muchbeer <gman2them@gmail.com>
2020-05-21 14:59:23 +0300

    Initial commit

---
+++ app/src/main/java/raum/muchbeer/bottomnavigationstackoverflow/MainActivity.java
@@ -0,0 +1,27 @@
+package raum.muchbeer.bottomnavigationstackoverflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+}
